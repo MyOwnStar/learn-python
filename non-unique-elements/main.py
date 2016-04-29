fe5a4e2f6472fc68f39b7b9a698930261500307e
MyOwnStar <mashenkoalex@gmail.com>
2016-04-29 10:08:50 +0900

    Done

--- non-unique-elements/main.py
+++ non-unique-elements/main.py
@@ -11,3 +11,20 @@ checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
 checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
 """
 
+
+def checkio(array):
+    result = []
+    for x in array:
+        if array.count(x) > 1:
+            result.append(x)
+    return result
+
+print(checkio([1, 2, 3, 1, 3]))
+print(checkio([1, 2, 3, 4, 5]))
+print(checkio([5, 5, 5, 5, 5]))
+print(checkio([10, 9, 10, 10, 9, 8]))
+
+
+
+
+
