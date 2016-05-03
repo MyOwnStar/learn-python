241c4d1221bac11d3a14010eb8fb80fa5d9ffbc3
Alexander Mashchenko <mashenkoalex@gmail.com>
2016-05-03 18:25:15 +0900

    Median was found :D

--- median/main.py
+++ median/main.py
@@ -12,3 +12,19 @@ checkio([3, 1, 2, 5, 3]) == 3
 checkio([1, 300, 2, 200, 1]) == 2
 checkio([3, 6, 20, 99, 10, 15]) == 12.5
 """
+
+
+def checkio(array):
+    sorted_array = sorted(array)
+    mid = int(len(sorted_array) / 2)
+    if len(array) % 2 == 0:
+        return (sorted_array[mid] + sorted_array[mid - 1]) / 2
+    else:
+        return sorted_array[mid]
+
+
+
+print(checkio([1, 2, 3, 4, 5]))
+print(checkio([3, 1, 2, 5, 3]))
+print(checkio([1, 300, 2, 200, 1]))
+print(checkio([3, 6, 20, 99, 10, 15]))
